2d3bf62e380bf2df7bf89303e4dc6be3b4b8ef0d
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-06-15 18:36:20 +0200

    Update posts.js

--- app/(tabs)/posts.js
+++ app/(tabs)/posts.js
@@ -1,34 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import Post from "../components/Post";
 
 export default function Posts() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        async function getPosts() {
+            const response = await fetch("https://post-rest-api-default-rtdb.firebaseio.com/posts.json");
+            const dataObj = await response.json();
+            const postsArray = Object.keys(dataObj).map(key => ({ id: key, ...dataObj[key] })); // from object to array
+            setPosts(postsArray);
+        }
+        getPosts();
+    }, []);
+
+    function renderPosts(item) {
+        const post = item.item;
+        return <Post post={post} />;
+    }
+
     return (
-        <View style={styles.container}>
-            <View style={styles.main}>
-                <Text style={styles.title}>Posts</Text>
-                <Text style={styles.subtitle}>This is the first page of your app.</Text>
-            </View>
+        <View style={styles.list}>
+            <FlatList data={posts} renderItem={renderPosts} keyExtractor={post => post.id} />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        padding: 24
-    },
-    main: {
-        flex: 1,
-        justifyContent: "center",
-        maxWidth: 960,
-        marginHorizontal: "auto"
-    },
-    title: {
-        fontSize: 64,
-        fontWeight: "bold"
-    },
-    subtitle: {
-        fontSize: 36,
-        color: "#38434D"
+    list: {
+        backgroundColor: "RED",
+        flex: 1
     }
 });
