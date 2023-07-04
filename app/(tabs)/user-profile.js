841fa299b076e554c669af8bc6453f26d93d08db
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-07-04 15:17:03 +0200

    Location added

--- app/(tabs)/user-profile.js
+++ app/(tabs)/user-profile.js
@@ -3,15 +3,36 @@ import { useEffect, useState } from "react";
 import { Button, Platform, StyleSheet, View, Text } from "react-native";
 import { auth } from "../../firebase-config";
 import { signOut } from "firebase/auth";
+import * as Location from "expo-location";
 
 export default function UserProfile() {
     const router = useRouter();
     const [mail, setEmail] = useState([]);
+    const [location, setLocation] = useState(null);
+    const [long, setLong] = useState("");
+    const [lat, setLat] = useState("");
+    const [errorMsg, setErrorMsg] = useState(null);
     const API_URL = "https://expo-post-app-default-rtdb.firebaseio.com";
 
     useEffect(() => {
         console.log(auth.currentUser.email);
         setEmail(auth.currentUser.email);
+    }, []);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+                setErrorMsg("Permission to access location was denied");
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+            setLong(location.coords.longitude);
+            setLat(location.coords.latitude);
+            console.log(location);
+        })();
     }, []);
 
     async function handleSignOut() {
@@ -34,6 +55,8 @@ export default function UserProfile() {
             />
             <View>
                 <Text>{mail}</Text>
+                <Text>Lat: {lat}</Text>
+                <Text>Long: {long}</Text>
             </View>
         </View>
     );
