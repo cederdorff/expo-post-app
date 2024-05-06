4922bbb5eda9f23d6664e6dda81928fa3c449432
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-05-06 13:24:26 +0200

    ThemeVariables added

---
+++ constants/ThemeVariables.jsx
@@ -0,0 +1,9 @@
+const primary = "#264c59";
+const secondary = "#acc6c9";
+const tintColorLight = "#fff";
+const tintColorDark = primary;
+const borderRadius = 7;
+const borderWidth = 2;
+const labelFontSize = 20;
+
+export { primary, secondary, tintColorLight, tintColorDark, borderRadius, borderWidth, labelFontSize };
