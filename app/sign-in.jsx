42388ab857dac6d403a632b575ebe994ff3c7899
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-12-06 12:25:26 +0100

    Update sign-in.jsx

--- app/sign-in.jsx
+++ app/sign-in.jsx
@@ -1,90 +1,85 @@
 import {
   borderRadius,
   labelFontSize,
+  placeholderTextColor,
   primary,
   secondary,
   tintColorLight
 } from "@/constants/ThemeVariables";
 import { Stack, router } from "expo-router";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
-import { StyleSheet, Text, TextInput, ScrollView } from "react-native";
+import { Image, StyleSheet, Text, TextInput, ScrollView } from "react-native";
 import StyledButton from "../components/StyledButton";
-import { placeholderTextColor } from "../constants/ThemeVariables";
-import { auth } from "../firebaseConfig"; // Import the auth object from firebase
-import Loader from "../components/Loader";
 
 export default function SignIn() {
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
-  const [loading, setLoading] = useState(false);
+  const auth = getAuth();
 
-  async function handleSignIn() {
-    setLoading(true);
-    // Sign in with email and password
-    await signInWithEmailAndPassword(auth, mail, password)
+  function handleSignIn() {
+    signInWithEmailAndPassword(auth, mail, password)
       .then(userCredential => {
-        // User is signed in
-        const user = userCredential.user; // User
-        console.log("Signed in as", user.email);
-        router.replace("/"); // Redirect to home
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        router.replace("/");
       })
       .catch(error => {
-        // Handle errors
         let errorMessage = error.code.split("/")[1];
         errorMessage = errorMessage.replaceAll("-", " ");
         setMessage(errorMessage);
       });
-    setLoading(false);
   }
 
   function goToSignUp() {
-    router.push("/sign-up"); // Redirect to sign up
+    router.push("/sign-up");
   }
 
   return (
-    <>
-      <ScrollView
-        style={styles.container}
-        automaticallyAdjustKeyboardInsets={true}>
-        <Stack.Screen
-          options={{
-            title: "Sign In",
-            headerTintColor: tintColorLight,
-            headerStyle: {
-              backgroundColor: primary
-            }
-          }}
-        />
-        <Text style={styles.label}>Mail</Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={setMail}
-          value={mail}
-          placeholder="Type your mail"
-          placeholderTextColor={placeholderTextColor}
-          autoCapitalize="none"
-        />
-        <Text style={styles.label}>Password</Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={setPassword}
-          value={password}
-          secureTextEntry={true}
-          placeholder="Type your password"
-          placeholderTextColor={placeholderTextColor}
-        />
-        <Text style={styles.errorMessage}>{message}</Text>
-        <StyledButton text="Sign In" onPress={handleSignIn} style="primary" />
-        <StyledButton
-          text="Create New Account"
-          onPress={goToSignUp}
-          style="secondary"
-        />
-      </ScrollView>
-      <Loader show={loading} />
-    </>
+    <ScrollView
+      style={styles.container}
+      automaticallyAdjustKeyboardInsets={true}>
+      <Stack.Screen
+        options={{
+          title: "Sign In",
+          headerTintColor: tintColorLight,
+          headerStyle: {
+            backgroundColor: primary
+          }
+        }}
+      />
+      <Image
+        source={require("@/assets/images/icon.png")}
+        style={styles.image}
+      />
+      <Text style={styles.label}>Mail</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setMail}
+        value={mail}
+        placeholder="Type your mail"
+        placeholderTextColor={placeholderTextColor}
+        autoCapitalize="none"
+      />
+      <Text style={styles.label}>Password</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+        secureTextEntry={true}
+        placeholder="Type your password"
+        placeholderTextColor={placeholderTextColor}
+      />
+      <Text style={styles.errorMessage}>{message}</Text>
+      <StyledButton text="Sign In" onPress={handleSignIn} style="primary" />
+      <StyledButton
+        text="Create New Account"
+        onPress={goToSignUp}
+        style="secondary"
+      />
+    </ScrollView>
   );
 }
 
@@ -94,16 +89,16 @@ const styles = StyleSheet.create({
     padding: 24,
     backgroundColor: secondary
   },
-  main: {
-    flex: 1
-  },
+
   image: {
-    aspectRatio: 1
+    aspectRatio: 1,
+    height: 300,
+    alignSelf: "center"
   },
   label: {
     fontSize: labelFontSize,
     color: primary,
-    marginTop: 30,
+    marginTop: 16,
     marginBottom: 5
   },
   input: {
